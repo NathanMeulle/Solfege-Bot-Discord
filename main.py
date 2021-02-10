53daf604ba55ad077c1c651682044aa160489bf8
NathanMeulle <54913885+NathanMeulle@users.noreply.github.com>
2021-02-10 22:01:59 +0100

    heroku test

--- main.py
+++ main.py
@@ -11,7 +11,9 @@
 
 
 from JAC import Bot
-
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+print(s3)
 
 if __name__== "__main__":
     bot = Bot()
